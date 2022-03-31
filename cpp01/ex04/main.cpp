0c106d19ba2e9575cdcc4cc70da2193873713d59
Marcos Viudes <mviudes@student.42madrid.com>
2022-03-31 17:58:05 +0200

    finished cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,21 +1,58 @@
 #include <iostream>
 #include <string>
-#include "Replace.hpp"
+#include <fstream>
+
+static std::string	replaceLine(std::string line, std::string search, std::string replace)
+{
+	size_t	offset;
+	size_t	start;
+	size_t	end;
+	std::string	newline;
+
+	start = line.find(search);
+	offset = 0;
+	while (start != line.npos) {
+		end = start + search.size();
+		newline += (line.substr(offset, start) + replace);
+		offset += end;
+		start = line.substr(offset).find(search);
+	}
+	newline += line.substr(offset);
+	if (!newline.size())
+		return (line);
+	return (newline);
+}
 
 int main(int argc, char **argv)
 {
-	(void)argv;
-	Replace	replace;
-
+	std::ifstream input;
+	std::ofstream output;
+	std::string		filename;
+	std::string		line;
+	
 	if(argc == 4)
 	{
-		replace.setFilename(argv[1]);
-		replace.setString1(argv[2]);
-		replace.setString2(argv[3]);
-		std::cout << "tiene 3 argumentos" << std::endl;
-		std::cout << "String 1: " << replace.getString1() << std::endl;
-		std::cout << "String 2: " << replace.getString2() << std::endl;
-		std::cout << "filename: " << replace.getFilename() << std::endl;
+		filename = argv[1];
+		input.open(argv[1]);
+		output.open(filename.append(".replace"));
+		if(!input.good() || !output.good())
+		{
+			output.close();
+			input.close();
+			std::cout << "File error" << std::endl;
+			return(-1);
+		}
+		if(((std::string)argv[1]).empty() || ((std::string)argv[2]).empty())
+		{
+			std::cout << "Error: empty args are not valid" << std::endl;
+			return(-2);
+		}
+		while (std::getline(input, line))
+			output << replaceLine(line, argv[2], argv[3]) << std::endl;
+		output.close();
+		input.close();
+		return(0);
 	}
-	return(0);
+		std::cout << "Run: " << argv[0] << " [Filename] [To_replace] [Replace]" << std::endl;
+	return(1);
 }
